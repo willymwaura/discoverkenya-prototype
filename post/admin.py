bb9c382495c2cfae72bbff5fee32b473dbdc4239
willymwaura <wilsonmwaura697@gmail.com>
2022-05-18 19:38:57 +0900

    foreign keys in models

--- post/admin.py
+++ post/admin.py
@@ -1,8 +1,9 @@
 
 from django.contrib import admin
-from .models import Feature
+from .models import Feature,Region
 
 
 # Register your models here.
 
 admin.site.register(Feature)
+admin.site.register(Region)
--- post/models.py
+++ post/models.py
@@ -1,13 +1,22 @@
+from tkinter import CASCADE
 from django.db import models
 from datetime import datetime
 
 from pytz import timezone
 
 # Create your models here.
+class Region(models.Model):
+    region=models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.region
 class Feature(models.Model):
-    region=models.CharField(max_length=100,default ='central')
+    region=models.ForeignKey(Region,related_name='regions',on_delete=models.CASCADE)
     title=models.CharField(max_length=100)
     experience=models.CharField(max_length=100)
     created=models.DateTimeField(default=datetime.now,blank=True)
     location=models.CharField(max_length=100,default="nairobi")
     weather=models.CharField(max_length=100,default="hot")
+    def __str__(self):
+        return '{}{}'.format(self.region,self.title)
+
